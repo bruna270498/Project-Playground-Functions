45f194a1a62ecace92a2c6167ece99004690e98b
Bruna <brunaalmeida075@gmail.com>
2022-08-25 09:38:13 -0300

    atualização de exercicio 8 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -37,30 +37,59 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let max = Math.max(...array)
+  let quantidade = 0;
+  for( let index = 0; index< array.length;index+=1){
+      if(array[index]=== max){
+        quantidade += 1
+      }
+  }
+ return quantidade;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let poscat1 = Math.abs(mouse - cat1);
+  let poscat2 = Math.abs(mouse - cat2);
+  if (poscat1 < poscat2) {
+    return 'cat1';
+  } else if (poscat1 > poscat2) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   // seu código aqui
+  let compactador= [];
+  for( let index of array){
+    if(index%3===0 && index%5===0){
+      compactador.push('fizzBuzz');
+    }else if(index % 3===0){
+      compactador.push('fizz');
+    }else if(index %5===0){
+      compactador.push('buzz');
+    }else{compactador.push('bug!');}
+  }
+  return compactador;
 }
 
+
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+
+
 }
 function decode() {
   // seu código aqui
 }
 
 // Desafio 10
-function techList() {
+function techList(array, string) {
   // seu código aqui
 }
 
